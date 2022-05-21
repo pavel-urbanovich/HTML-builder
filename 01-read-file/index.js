073d5b207031add86f1e423fe3bd6318b02e6da8
Pavel Urbanovich <yrbik@mail.ru>
2022-05-21 11:23:36 +0300

    fix: add solution 01-read-file with stream

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readFile(
-  path.join('01-read-file', 'text.txt'),
-  'utf-8',
-  (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  }
-);
+const readableStream = fs.createReadStream(path.join('01-read-file', 'text.txt'), 'utf-8');
+
+let data = '';
+
+readableStream.on('data', chunk => data += chunk);
+readableStream.on('end', () => console.log(data));
+readableStream.on('error', error => console.log('Error', error.message));
